8aacc2983f595ee6d5e2f3cb916f7a6eb396c627
Hippolyte-Gander <165796324+Hippolyte-Gander@users.noreply.github.com>
2024-04-22 16:59:29 +0200

    Add files via upload

---
+++ film.php
@@ -0,0 +1,37 @@
+<?php
+
+new Film {
+    private string $titreFilm;
+    private DateTime $dateSortie;
+    private int $dureeFilm;
+    private string $synopsis;
+
+    public function __construct(string $titreFilm, DateTime $dateSortie, int $dureeFilm) {
+        $this->titreFilm = $titreFilm;
+        $this->dateSortie = $dateSortie;
+        $this->dureeFilm = $dureeFilm;
+        $this->synopsis = ();
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+
+
+
+
+?>
